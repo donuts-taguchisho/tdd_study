43e82d17621af6212d3303fe820a6c19e6015314
donuts-taguchisho <taguchi.sho@donuts.ne.jp>
2025-08-19 12:39:10 +0900

    12

--- xUnit/xunit.py
+++ xUnit/xunit.py
@@ -1,6 +1,9 @@
 class TestCase:
     def __init__(self, name):
         self.name = name
+    def run(self):
+        method = getattr(self, self.name)
+        method()
 
 test = WasRun("testMethod")
 print(test.wasRun)
@@ -11,8 +14,5 @@ class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None
         super().__init__(name)
-    def run(self):
-        method = getattr(self, self.name)
-        method()
     def testMethod(self):
         self.wasRun = 1

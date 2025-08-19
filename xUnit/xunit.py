afad70d213ec124a9ea1f0e0dc50152ea2ec11bf
donuts-taguchisho <taguchi.sho@donuts.ne.jp>
2025-08-19 12:41:51 +0900

    #18 xUnitへ向かう小さな一歩

--- xUnit/xunit.py
+++ xUnit/xunit.py
@@ -4,15 +4,18 @@ class TestCase:
     def run(self):
         method = getattr(self, self.name)
         method()
-
-test = WasRun("testMethod")
-print(test.wasRun)
-test.run()
-print(test.wasRun)
-
 class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None
         super().__init__(name)
     def testMethod(self):
         self.wasRun = 1
+
+class TestCaseTest(TestCase):
+    def testRunning(self):
+        test = WasRun("testMethod")
+        assert not test.wasRun
+        test.run()
+        assert test.wasRun
+
+TestCaseTest("testRunning").run()

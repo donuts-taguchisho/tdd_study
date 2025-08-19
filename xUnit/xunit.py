07260fd476243840159f835ce2a26a72b13bddfc
donuts-taguchisho <taguchi.sho@donuts.ne.jp>
2025-08-19 13:01:28 +0900

    5

--- xUnit/xunit.py
+++ xUnit/xunit.py
@@ -20,7 +20,6 @@ class WasRun(TestCase):
 class TestCaseTest(TestCase):
     def testRunning(self):
         test = WasRun("testMethod")
-        assert not test.wasRun
         test.run()
         assert test.wasRun
     def testSetUp(self):

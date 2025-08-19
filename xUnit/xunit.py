5db6e3fb0260a1f08f7465fe9695377f64d0ecac
donuts-taguchisho <taguchi.sho@donuts.ne.jp>
2025-08-19 12:59:42 +0900

    3

--- xUnit/xunit.py
+++ xUnit/xunit.py
@@ -1,9 +1,15 @@
 class TestCase:
     def __init__(self, name):
         self.name = name
+    def setUp(self):
+        pass
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
+        self.tearDown()
+    def tearDown(self):
+        pass
 class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None

9bc469b576ac7b207e9e4df0fbbdcdb3089a3271
donuts-taguchisho <taguchi.sho@donuts.ne.jp>
2025-08-19 12:36:30 +0900

    9

--- xUnit/xunit.py
+++ xUnit/xunit.py
@@ -6,7 +6,9 @@ print(test.wasRun)
 class WasRun:
     def __init__(self, name):
         self.wasRun = None
+        self.name = name
+    def run(self):
+        method = getattr(self, self.name)
+        method()
     def testMethod(self):
         self.wasRun = 1
-    def run(self):
-        self.testMethod()
